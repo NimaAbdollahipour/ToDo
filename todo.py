36a5bd2bd6582279b99c8c60f71f02a0524d124e
Nima Abdollahipour <nima.abdollahi.a@gmail.com>
2023-01-21 19:07:33 +0200

    mark as done and delete done

--- todo.py
+++ todo.py
@@ -2,15 +2,15 @@ import typer
 from datetime import date
 import sqlite3
 from colorama import init as colorama_init
-from colorama import Fore
+from colorama import Fore,Back
 from colorama import Style
 
-
+COLORS = [Fore.BLACK+Back.RED, Back.YELLOW+Fore.BLACK, Back.GREEN+Fore.BLACK, Back.WHITE+Fore.BLUE]
 IMPORTANCE = {'high':1,'medium':2,'low':3}
 colorama_init()
 con = sqlite3.connect("database.db")
 cur = con.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS todos(id INTEGER PRIMARY KEY, title TEXT, created DATE, date DATE, type INTEGER)')
+cur.execute('CREATE TABLE IF NOT EXISTS todos(id INTEGER PRIMARY KEY, title TEXT, created DATE, date DATE, importance INTEGER, done INTEGER)')
 app = typer.Typer()
 
 
@@ -20,25 +20,24 @@ def add(title: str,for_date:str,importance:str):
     converted_date = convert_to_date(for_date)
     if importance and converted_date:
         try:
-            cur.execute("INSERT INTO todos (title, created, date, type) VALUES (?, ?, ?, ?)", (title,date.today(),converted_date, importance))
+            cur.execute("INSERT INTO todos (title, created, date, importance, done) VALUES (?, ?, ?, ?, 0)", (title,date.today(),converted_date, importance))
             con.commit()
+            disp_success('todo added successfully')
         except:
             disp_error('could not save to database')
-    disp_success('todo added successfully')
-
+    
 
 @app.command()
 def list(day: str):
     results = []
     if(day=='today'):
-        cur.execute("SELECT * FROM todos WHERE date=? ORDER BY type",(date.today(),))
+        cur.execute("SELECT * FROM todos WHERE date=? ORDER BY importance",(date.today(),))
         results = cur.fetchall()
     elif(day=='all'):
         cur.execute("SELECT * FROM todos ORDER BY date")
         results = cur.fetchall()
     for result in results:
         display(result)
-
 
 @app.command()
 def edit(id: int,field:str,value):
@@ -48,47 +47,85 @@ def edit(id: int,field:str,value):
     if(field=='title'):
         cur.execute("UPDATE todos SET title=? WHERE id=?",(value,id))
     elif(field=='date'):
-        cur.execute("UPDATE todos SET date=? WHERE id=?",(value,id))
-    elif(field=='type' or field=='importance'):
-        cur.execute("UPDATE todos SET type=? WHERE id=?",(value,id))
+        value = convert_to_date(value)
+        if value:
+            cur.execute("UPDATE todos SET date=? WHERE id=?",(value,id))
+    elif(field=='importance'):
+        value = importance_to_num(value)
+        if value:
+            cur.execute("UPDATE todos SET importance=? WHERE id=?",(value,id))
     else:
-        print(f'{Fore.RED}wrong field{Style.RESET_ALL}')
+        disp_error('wrong field')
         return
     con.commit()
-    print(f'{Fore.GREEN}successfully updated to:{Style.RESET_ALL}')
+    disp_success('updated successfully')
     cur.execute("SELECT * FROM todos WHERE id=?",(id,))
     result = cur.fetchone()
     display(result)
-
 
 @app.command()
 def delete(id: int):
     cur.execute("SELECT * FROM todos WHERE id=?",(id,))
     result = cur.fetchone()
     if(not result):
-        print(f'{Fore.RED}not found{Style.RESET_ALL}')
+        disp_error('not found')
         return
     display(result)
     try:
         cur.execute("DELETE FROM todos WHERE id=?",(id,))
         con.commit()
-        print(f'{Fore.GREEN}successfully deleted:{Style.RESET_ALL}')
+        disp_success('deleted successfully')
     except:
-        print(f'{Fore.RED}could not delete:{Style.RESET_ALL}')
+        disp_error('failed to delete')
+
+@app.command()
+def done(id: int):
+    cur.execute("SELECT * FROM todos WHERE id=?",(id,))
+    result = cur.fetchone()
+    if(not result):
+        disp_error('not found')
+        return
+    display(result)
+    try:
+        cur.execute("UPDATE todos SET done=1 WHERE id=?",(id,))
+        con.commit()
+        disp_success('marked as done')
+    except:
+        disp_error('failed to mark as done')
+
+@app.command()
+def undone(id: int):
+    cur.execute("SELECT * FROM todos WHERE id=?",(id,))
+    result = cur.fetchone()
+    if(not result):
+        disp_error('not found')
+        return
+    display(result)
+    try:
+        cur.execute("UPDATE todos SET done=0 WHERE id=?",(id,))
+        con.commit()
+        disp_success('marked as undone')
+    except:
+        disp_error('failed to mark as undone')
+
+@app.command()
+def deletedone():
+    try:
+        cur.execute("DELETE FROM todos WHERE done=1")
+        con.commit()
+        disp_success('deleted done todos')
+    except:
+        disp_error('failed to delete')
 
 
 def display(result):
-    if(result[4]==1):
-        print(Fore.RED, end='')
-    elif(result[4]==2):
-        print(Fore.YELLOW, end='')
-    elif(result[4]==3):
-        print(Fore.GREEN,  end='')
+    if result[5]==1:
+        color = COLORS[3] + ' \u2713 '
     else:
-        print(Fore.BLUE, end='')
+        color = COLORS[result[4]-1] + ' \u2717 '
     id = str(result[0])
     id = (4-len(id))*'0'+id
-    print(id,':',result[3],':',result[1]+Style.RESET_ALL)
+    print(f'{color} {id}: {result[3]} : {result[1]} {Style.RESET_ALL}')
 
 
 def disp_error(msg):
